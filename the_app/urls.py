4f9a1e176f7311c4826add9bae21205b9e44e904
Michael Maranan <michaelmaranan030@gmail.com>
2021-10-29 09:34:33 +0800

    DetailView and ListView

--- the_app/urls.py
+++ the_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
-# from .views import PersonList
+from .views import PersonList, PersonDetail
 
 urlpatterns = [
-	# path('', PersonList.as_view(), name='personlist'),
-	path('', views.persons, name='persons')	
+	path('', PersonList.as_view(), name='personlist'),
+	path('pd/<int:pk>', PersonDetail.as_view(), name='persondetail'),
 ]
--- the_app/views.py
+++ the_app/views.py
@@ -2,8 +2,20 @@ from django.shortcuts import render,redirect
 from .forms import PersonForm
 from .models import Person
 from django.views.generic import View
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 # Create your views here.
+class PersonList(ListView):
+	model = Person
+	template_name = 'the_app/home.html'
+	context_object_name = 'person_list'
+
+class PersonDetail(DetailView):
+	model = Person
+	context_object_name = 'person'
+
+
 # class PersonList(View):
 # 	def get(self,request):
 # 		people = Person.objects.all()
@@ -13,13 +25,13 @@ from django.views.generic import View
 # 	def post(self,request):
 # 		pass
 
-def persons(request):
-	person_list = Person.objects.all()
-	my_form = PersonForm()
-	if request.method == 'POST':
-		my_form = PersonForm(request.POST)
-		if my_form.is_valid():
-			my_form.save()
-			return redirect('persons')
-	context = {"person_list":person_list,"my_form":my_form}
-	return render(request, 'the_app/home.html', context)
+# def personList(request):
+# 	person_list = Person.objects.all()
+# 	my_form = PersonForm()
+# 	if request.method == 'POST':
+# 		my_form = PersonForm(request.POST)
+# 		if my_form.is_valid():
+# 			my_form.save()
+# 			return redirect('persons')
+# 	context = {"person_list":person_list,"my_form":my_form}
+# 	return render(request, 'the_app/home.html', context)
