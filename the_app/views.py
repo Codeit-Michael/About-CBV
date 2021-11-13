41493cda12d6094fc60010e67500c7c9584e75de
Michael Maranan <michaelmaranan030@gmail.com>
2021-11-13 10:31:24 +0800

    fixed getting requests

--- the_app/views.py
+++ the_app/views.py
@@ -36,5 +36,27 @@ class PersonDetail(DetailView):
 		return super(PersonDetail, self).get(*args,**kwargs)
 
 	def post(self,*args,**kwargs):
-		pass
+		return super(PersonDetail, self).get(*args,**kwargs)
+
+	def dispatch(self,request,*args,**kwargs):
+		if self.request.POST.get('save'):
+			print(self.request)
+			print(True)
+		return super(PersonDetail, self).dispatch(request,*args,**kwargs)
+
+		# if request.POST.get('save'):
+		# 	for item in mylist.item_set.all():
+		# 		if request.POST.get(f'c{item.id}') == 'clicked':
+		# 			item.is_complete = True
+		# 		else:
+		# 			item.is_complete = False
+		# 		item.save()
+		# elif request.POST.get('addItem'):
+		# 	new = request.POST.get('newItem')
+		# 	mylist.item_set.create(text=new, is_complete=False)
+		# elif request.POST.get('delThis'):
+		# 	item_index = request.POST.get('delThis')
+		# 	mylist.item_set.get(id=item_index).delete()
+
+		# return super(PersonDetail, self)
 		# WORK ON GIVING BOOLEAN (UPDATEVIEW) & CREATING NEW ONE (CREATEVIEW)
