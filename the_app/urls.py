e8ce768e0a078b6ebb40cfa334f178de3befa57a
Michael Maranan <michaelmaranan030@gmail.com>
2022-02-07 13:45:29 +0800

    reconnect logout to login

--- the_app/urls.py
+++ the_app/urls.py
@@ -5,7 +5,8 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
 	path('signup/', UserSignup.as_view(), name='signup'),
 	path('login/', CustomLoginView.as_view(), name='login'),
-	path('logout/', LogoutView.as_view(next_page='signin'), name='logout'),
+	# path('accounts/login/?next=/', CustomLoginView.as_view(), name='login'),
+	path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 	path('', PersonList.as_view(), name='personlist'),
 	path('person-delete/pk=<int:pk>', PersonDelete.as_view(), name='persondelete'),
 	path('person-detail/pk=<int:pk>', PersonDetail.as_view(), name='persondetail'),
